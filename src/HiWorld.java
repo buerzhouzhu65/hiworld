ad3a30dc738aa8424ac5e2383ebfb732d1058c02
zhifengShe <zhifengshe0920@163.com>
2020-04-17 13:57:47 +0800

    第一次修改

--- src/HiWorld.java
+++ src/HiWorld.java
@@ -2,6 +2,6 @@ public class HiWorld {
     public static void main(String[] args) {
         System.out.print("hi worldII");
         System.out.println("hi Java");
-
+        System.out.println("第一次修改");
     }
 }
